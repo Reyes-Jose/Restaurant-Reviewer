d2e28b50f1843fea837635b8efc201cf364941a4
JoeDoom <reyesj1209@gmail.com>
2022-09-20 19:10:22 -0700

    adds review.js file

---
+++ models/Review.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Review extends Model{}
+
+Review.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,        
+    },
+    comment:{
+        type: Datatypes.STRING
+    },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+}
+);
