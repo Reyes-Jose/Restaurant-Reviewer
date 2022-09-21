8d4194bcd18c30ebaf2866af172bbad7f4dc14b6
Ryan Whitmore <ryan.whit@outlook.com>
2022-09-20 20:36:06 -0700

    [#5] Added restaurant routes and udpated Review routes to include restaurant relationship.

---
+++ routes/api/reviewRestaurants.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { Restaurant, Review } = require('../../models');
+
+// GET all restaurant
+router.get('/', async (_req, res) => {
+  try {
+    const restaurant = await Restaurant.findAll({
+      include: [{ model: Review}],
+    });
+    res.status(200).json(restaurant);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single restaurant by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id, {
+      include: [{ model: Review}],
+    });
+
+    if (!restaurant) {
+      res.status(404).json({ message: 'No restaurant found with this id!' });
+      return;
+    }
+
+    res.status(200).json(restaurant);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// CREATE a restaurant
+router.post('/', async (req, res) => {
+  try {
+    const restaurant = await Restaurant.create(req.body);
+    res.status(200).json(restaurant);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// DELETE a restaurant
+router.delete('/:id', async (req, res) => {
+  try {
+    const restaurant = await Restaurant.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!restaurant) {
+      res.status(404).json({ message: 'No restaurant found with this id!' });
+      return;
+    }
+
+    res.status(200).json(restaurant);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
